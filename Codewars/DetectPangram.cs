1421cc4b679e447376b595de5d12cb2b1b0e86d2
foxesknow <seanbatten58@gmail.com>
2025-03-30 16:20:06 +0100

    Pangrams

---
+++ Codewars/DetectPangram.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codewars;
+
+public class DetectPangram
+{
+    public static bool IsPangram(string str)
+    {
+        var count = 0;
+        Span<bool> seen = stackalloc bool[26];
+
+        for(int i = 0, length = str.Length; i < length; i++)
+        {
+            var c = char.ToLower(str[i]);
+            if(c >= 'a' && c <= 'z')
+            {
+                var asInt = c - 'a';
+                if(seen[asInt] == false)
+                {
+                    count++;
+                    seen[asInt] = true;
+
+                    if(count == 26) return true;
+                }
+            }
+        }
+
+        return false;
+    }
+}
---
+++ Codewars/DetectPangramTests.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using NUnit.Framework;
+
+namespace Codewars;
+
+[TestFixture]
+public class DetectPangramTests
+{
+    [Test]
+    public void SampleTests() 
+    {
+      Assert.That(DetectPangram.IsPangram("The quick brown fox jumps over the lazy dog."), Is.True);
+    }
+
+    [Test]
+    [TestCase("hello")]
+    [TestCase("abcdefghijklmnopqrstuvwxy")]
+    public void NotPangram(string value) 
+    {
+      Assert.That(DetectPangram.IsPangram(value), Is.False);
+    }
+
+    [Test]
+    [TestCase("abcdefghijklmnopqrstuvwxyz")]
+    public void IsPangram(string value) 
+    {
+      Assert.That(DetectPangram.IsPangram(value), Is.True);
+    }
+}
