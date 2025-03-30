a56dafc3ba1983fbc5e9834f8b41395de1f44742
foxesknow <seanbatten58@gmail.com>
2025-03-30 16:09:24 +0100

    JadenCase

---
+++ Codewars/JadenCase.cs
@@ -0,0 +1,29 @@
+﻿using System.Text;
+
+namespace Codewars;
+
+public static class JadenCase
+{
+    public static string ToJadenCase(this string phrase)
+    {
+        if(string.IsNullOrWhiteSpace(phrase)) return phrase;
+
+        var b = new StringBuilder(phrase.Length);
+        b.Append(char.ToUpper(phrase[0]));
+
+        for(int i = 1, length = phrase.Length; i < length; i++)
+        {
+            var c = phrase[i];
+            if(phrase[i - 1] == ' ')
+            {
+                b.Append(char.ToUpper(c));
+            }
+            else
+            {
+                b.Append(c);
+            }
+        }        
+
+        return b.ToString();
+    }
+}
---
+++ Codewars/JadenCaseTests.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using NUnit.Framework;
+
+namespace Codewars;
+
+[TestFixture]
+public class JadenCaseTest
+{
+    [Test]
+    public void FixedTest()
+    {
+        Assert.That("How can mirrors be real if our eyes aren't real".ToJadenCase(), Is.EqualTo("How Can Mirrors Be Real If Our Eyes Aren't Real"), "Strings didn't match.");
+    }
+
+    [Test]
+    [TestCase("hello", "Hello")]
+    [TestCase(" hello", " Hello")]
+    [TestCase("  hello  ", "  Hello  ")]
+    public void SingleWord(string value, string expected)
+    {
+        Assert.That(value.ToJadenCase(), Is.EqualTo(expected));
+    }
+
+    [Test]
+    [TestCase("hello world", "Hello World")]
+    public void TwoWords(string value, string expected)
+    {
+        Assert.That(value.ToJadenCase(), Is.EqualTo(expected));
+    }
+}
